60d94a8ebf94fae97dedc0d4930a3d65f58b40c5
wissalcodes <lw_messikh@esi.dz>
2024-10-05 17:14:08 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="text-title bg-bg-white text-title-desktop flex flex-col items-center justify-center min-h-screen w-screen font-poppins font-bold">
+      Welcome to the SEC Website :D
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        montserrat: ["Montserrat", "sans-serif"],
+        poppins: ["Poppins", "sans-serif"],
+      },
+      colors: {
+        "bg-white": "#F2F1FC",
+        "blue-400": "#7CA0FC",
+        title: "#1E0F2D",
+        "text-light": "#1E0F2DB2",
+        text: "#1E0F2D",
+        pink: "#C653FB",
+        "purple-50": "#CCBAFE",
+        "purple-100": "#CCB9FF",
+        "purple-200": "#D298ED",
+        "turquoise-50": "#96E1F0",
+        "blue-300": "#8AAAFD",
+        "navbar-white": "#FFFFFFE5",
+        purple: "#1E0F2D",
+      },
+      fontSize: {
+        "title-desktop": "64px",
+        "title-mobile": "32px",
+        "text-desktop": "18px",
+        "text-mobile": "12px",
+      },
+    },
+  },
+  plugins: [],
+};
