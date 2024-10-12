84b30bb3b9c82f3e3c8827c21bac997aa8252d81
Khadidja225 <mk_brakta@esi.dz>
2024-10-12 13:32:04 +0100

    done

--- src/components/OurActivities/Card.jsx
+++ src/components/OurActivities/Card.jsx
@@ -2,6 +2,8 @@ import React from "react";
 
 function Card({
   icon,
+  index,
+  hoveredCardIndex,
   title,
   bg,
   descriptiontoshow,
@@ -13,20 +15,31 @@ function Card({
 }) {
   return (
     <div
-      className={`${bg} p-4 rounded-lg transition-all duration-300 ease-in-out`}
+      className={`${bg} z-0 p-4 rounded-lg transition-all duration-700 ease-in-out
+        absolute w-[calc(50%-1rem)] 
+        ${index % 2 === 0 ? 'left-0' : 'right-0'}
+        ${isHovered ? 'h-[400px] ' : shouldHideIcon ? 'h-[200px]' : 'h-[300px]'}
+        ${index < 2 ? 'top-0' : 'bottom-0'}
+        ${isHovered && index >= 2 ? '-translate-y-[0px]' : ''}
+        ${shouldHideIcon && index < 2 ? 'translate-y-[0px]' : ''}
+        overflow-hidden`}
       onMouseEnter={onHover}
       onMouseLeave={onLeave}
     >
-      <div className="flex flex-col items-center">
+      <div className="flex flex-col items-center h-full">
         <h3 className="text-xl font-bold mb-2">{title}</h3>
-        <p className="text-center text-lg">
-          {isHovered ? descriptionwhilehover : descriptiontoshow}
+        <p className="text-center md:text-lg text-sm transition-all duration-700">
+          {descriptiontoshow}
+          {isHovered &&
+          <span className={` mt-2 transition-all duration-300 ease-in-out`}>
+            {descriptionwhilehover}
+          </span>}
         </p>
         {!shouldHideIcon && (
           <img
             src={icon}
             alt={title}
-            className={` mb-4 transition-opacity duration-300 ease-in-out `}
+            className={` w-48 h-48 mt-auto transition-all duration-300 ease-in-out ${isHovered ? "scale-110" : ""}`}
           />
         )}
       </div>
--- src/sections/OurActivities.jsx
+++ src/sections/OurActivities.jsx
@@ -1,30 +1,29 @@
-import React, { useState } from "react";
-import Card from "../components/OurActivities/Card";
+import { useState } from "react";
+import Card from "../components/OurActivities/Card"
 import icon1 from "../assets/OurActivities/icon1.svg";
 import icon2 from "../assets/OurActivities/icon2.svg";
 import icon3 from "../assets/OurActivities/icon3.svg";
 import icon4 from "../assets/OurActivities/icon4.svg";
-
 function OurActivities() {
   const [hoveredCard, setHoveredCard] = useState(null);
 
   const cardData = [
     {
-      icon: icon1,
+      icon: icon2,
       title: 'Video games',
       bg: "bg-blue-200",
       descriptiontoshow: "Members enjoy energetic video game sessions,",
       descriptionwhilehover: "creating a fun and collaborative gaming experience that brings our community together in shared digital adventures."
     },
     {
-      icon: icon2,
+      icon: icon3,
       title: 'Board Games',
       bg: "bg-purple-50",
       descriptiontoshow: "Members enjoy energetic video game sessions,",
       descriptionwhilehover: "creating a fun and collaborative gaming experience that brings our community together in shared digital adventures."
     },
     {
-      icon: icon3,
+      icon: icon1,
       title: 'Internal Matches',
       bg: "bg-purple-200",
       descriptiontoshow: "Members engage in lively sessions featuring Ludo, Dominoes, and Monopoly",
@@ -40,13 +39,14 @@ function OurActivities() {
   ];
 
   return (
-    <div className="flex justify-center items-center py-8">
-      <div className="flex flex-col justify-center items-center w-3/5">
-        <p>Our Activities</p>
-        <div className="grid grid-cols-2 gap-8">
+    <div className="flex justify-center items-center py-8 z-0 px-8">
+      <div className="flex flex-col justify-center items-center lg:w-3/5 sm:w-4/5 w-full -z-0">
+        <p className="my-4">Our Activities</p>
+        <div className="relative h-[612px] w-full overflow-hidden">
           {cardData.map((card, index) => (
             <Card
               key={index}
+              index={index}
               icon={card.icon}
               title={card.title}
               bg={card.bg}
