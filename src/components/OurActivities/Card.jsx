0945559c7e0562e1528017b0f1f6f2aaf4190030
wissalcodes <lw_messikh@esi.dz>
2024-10-14 01:38:01 +0100

    added some styles

--- src/components/OurActivities/Card.jsx
+++ src/components/OurActivities/Card.jsx
@@ -11,40 +11,47 @@ function Card({
   isHovered,
   shouldHideIcon,
   onHover,
-  onLeave
+  onLeave,
 }) {
   return (
     <div
-      className={`${bg} z-0 p-4 rounded-lg transition-all duration-700 ease-in-out
-        absolute w-[calc(50%-1rem)] 
-        ${index % 2 === 0 ? 'left-0' : 'right-0'}
-        ${isHovered ? 'h-[400px] ' : shouldHideIcon ? 'h-[200px]' : 'h-[300px]'}
-        ${index < 2 ? 'top-0' : 'bottom-0'}
-        ${isHovered && index >= 2 ? '-translate-y-[0px]' : ''}
-        ${shouldHideIcon && index < 2 ? 'translate-y-[0px]' : ''}
+      className={`${bg} z-0 p-3 md:p-4 lg:p-7 rounded-[8.17px] transition-all duration-700 ease-in-out mx-[10px]
+        absolute max-w-[170px] md:max-w-full md:w-[calc(37%-1rem)] lg:w-[calc(49%-1rem)] xl:w-[calc(46%-1rem)] 2xl:w-[calc(43%-1rem)] xl:max-w-[300px] 2xl:max-w-[320px]
+        ${index % 2 === 0 ? "right-1/2" : "left-1/2"}
+        ${
+          isHovered
+            ? "h-[200px] md:h-[450px] "
+            : shouldHideIcon
+            ? "h-[80px] md:h-[150px]"
+            : "h-[139.78px] md:h-[300px]"
+        }
+        ${index < 2 ? "top-0" : "bottom-0"}
+        ${isHovered && index >= 2 ? "-translate-y-[0px]" : ""}
+        ${shouldHideIcon && index < 2 ? "translate-y-[0px]" : ""}
         overflow-hidden`}
       onMouseEnter={onHover}
-      onMouseLeave={onLeave}
-    >
-      <div className="flex flex-col items-start h-full">
-        <h3 className="sm:text-3xl text-xl text-start font-bold mb-4">{title}</h3>
-        <p className=" text-start md:text-lg text-sm transition-all duration-700">
+      onMouseLeave={onLeave}>
+      <div className="flex flex-col items-center h-full">
+        <h3 className="w-full text-purple font-[800] text-[14.26px] md:text-xl lg:text-2xl mb-2 font-montserrat text-start">
+          {title}
+        </h3>
+        <p className=" leading-[14.42px] text-start lg:text-sm 2xl:text-lg text-[10px] transition-all duration-700">
           {descriptiontoshow}
-          {isHovered &&
-          <span className={` mt-2 transition-all duration-300 ease-in-out`}>
-            {descriptionwhilehover}
-          </span>}
+          {isHovered && (
+            <span className={` mt-2 transition-all duration-300 ease-in-out`}>
+              {descriptionwhilehover}
+            </span>
+          )}
         </p>
-        <div className=" flex items-end justify-end w-full">
         {!shouldHideIcon && (
           <img
             src={icon}
             alt={title}
-            className={` md:w-48 md:h-48 w-28 h-28 mt-auto transition-all duration-300 ease-in-out ${isHovered ? "scale-110" : ""}`}
+            className={`w-[121.38px] md:w-48 md:h-48 mt-auto transition-all duration-300 ease-in-out ${
+              isHovered ? "scale-110" : ""
+            }`}
           />
         )}
-        </div>
-
       </div>
     </div>
   );
--- src/sections/OurActivities.jsx
+++ src/sections/OurActivities.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import Card from "../components/OurActivities/Card"
+import Card from "../components/OurActivities/Card";
 import icon1 from "../assets/OurActivities/icon1.svg";
 import icon2 from "../assets/OurActivities/icon2.svg";
 import icon3 from "../assets/OurActivities/icon3.svg";
@@ -10,39 +10,46 @@ function OurActivities() {
   const cardData = [
     {
       icon: icon2,
-      title: 'Video games',
+      title: "Video games",
       bg: "bg-blue-200",
       descriptiontoshow: "Members enjoy energetic video game sessions,",
-      descriptionwhilehover: "creating a fun and collaborative gaming experience that brings our community together in shared digital adventures."
+      descriptionwhilehover:
+        "creating a fun and collaborative gaming experience that brings our community together in shared digital adventures.",
     },
     {
       icon: icon3,
-      title: 'Board Games',
+      title: "Board Games",
       bg: "bg-purple-50",
-      descriptiontoshow: "Members enjoy energetic video game sessions,",
-      descriptionwhilehover: "creating a fun and collaborative gaming experience that brings our community together in shared digital adventures."
+      descriptiontoshow:
+        "Members engage in lively sessions featuring Ludo, Dominoes, and Monopoly",
+      descriptionwhilehover:
+        "creating a fun and collaborative gaming experience that brings our community together in shared digital adventures.",
     },
     {
       icon: icon1,
-      title: 'Internal Matches',
+      title: "Internal Matches",
       bg: "bg-purple-200",
-      descriptiontoshow: "Members engage in lively sessions featuring Ludo, Dominoes, and Monopoly",
-      descriptionwhilehover: "creating a fun and collaborative gaming experience that brings our community together in shared digital adventures."
+      descriptiontoshow:
+        "We organize matches that involve various ball sports ",
+      descriptionwhilehover: "This not only allows members to have fun ",
     },
     {
       icon: icon4,
-      title: 'Movie Night',
+      title: "Movie Night",
       bg: "bg-turquoise-50",
-      descriptiontoshow: "We plan weekly movie nights on our Discord server, enabling members to watch and discuss films simultaneously.",
-      descriptionwhilehover: "creating a fun and collaborative gaming experience that brings our community together in shared digital adventures."
-    }
+      descriptiontoshow: "We plan weekly movie nights on our Discord server,",
+      descriptionwhilehover:
+        "enabling members to watch and discuss films simultaneously. ",
+    },
   ];
 
   return (
     <div className="flex justify-center items-center py-8 z-0 px-8">
       <div className="flex flex-col justify-center items-center lg:w-3/5 sm:w-4/5 w-full -z-0">
-        <p className="my-4">Our Activities</p>
-        <div className="relative h-[612px] w-full overflow-hidden -z-0">
+        <p className="my-4 md:my-8 font-montserrat text-title-mobile lg:text-title-desktop font-[800]">
+          Our Activities
+        </p>
+        <div className="items-center justify-center relative h-[292.87px] md:h-[618px] flex flex-wrap  w-full overflow-hidden -z-0">
           {cardData.map((card, index) => (
             <Card
               key={index}
@@ -53,7 +60,11 @@ function OurActivities() {
               descriptiontoshow={card.descriptiontoshow}
               descriptionwhilehover={card.descriptionwhilehover}
               isHovered={hoveredCard === index}
-              shouldHideIcon={hoveredCard !== null && hoveredCard % 2 === index % 2 && hoveredCard !== index}
+              shouldHideIcon={
+                hoveredCard !== null &&
+                hoveredCard % 2 === index % 2 &&
+                hoveredCard !== index
+              }
               onHover={() => setHoveredCard(index)}
               onLeave={() => setHoveredCard(null)}
             />
