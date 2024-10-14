29f9118b6e5576cec47780dc6b00c8bc8825bd3e
wissalcodes <lw_messikh@esi.dz>
2024-10-14 16:04:44 +0100

    section merge fix

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,14 @@ import Navbar from "./components/layout/Navbar";
 import Events from "./sections/Events";
 import GetInTouch from "./sections/GetInTouch";
 import Hero from "./components/heroSection";
-
+import AboutUsPage from "./sections/AboutUs";
 import OurActivities from "./sections/OurActivities";
 function App() {
   return (
     <div className="z-0 relative text-title bg-white md:bg-bg-white text-title-desktop flex flex-col mx-auto md:min-h-screen w-screen font-poppins">
       <Navbar />
       <Hero />
+      <AboutUsPage />
       <Events />
       <OurActivities />
       <GetInTouch />
