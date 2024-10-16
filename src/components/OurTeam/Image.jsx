a09592bce463507d62e9292ecd5eedfdb9ecc641
wissalcodes <lw_messikh@esi.dz>
2024-10-16 11:10:21 +0100

    temp fix our team + goals

--- src/components/OurTeam/Image.jsx
+++ src/components/OurTeam/Image.jsx
@@ -1,31 +1,61 @@
-import React from "react";
+import React, { useEffect } from "react";
 
-const Image = ({ color, role, image, name, active }) => {
+const Image = ({ color, role, image, name, active, width, margin }) => {
+  useEffect(() => {}, []);
   return (
     <div
-      className="relative h-80 sm:h-[360px] w-full  rounded-xl"
-      style={{ backgroundColor: color }}>
+      className={` transition-all duration-500 md:scale-90 lg:scale-100 z-20 relative h-80 sm:h-[360px] flex rounded-xl 
+ 
+`}
+      style={{
+        backgroundColor: color,
+        width: width,
+      }}>
       <div
-        className="absolute bottom-0 left-0 w-full h-[35%] rounded-b-xl"
+        className={`absolute bottom-0 left-0 w-[${width}]  rounded-b-xl`}
         style={{
           background: "linear-gradient(rgba(30,15,45,0) 0%, #1E0F2D 100%)",
+          width: width,
         }}></div>
-      <img loading="lazy"  
+      <img
+        loading="lazy"
         src={image}
         alt={name}
-        className="object-cover rounded-xl w-full h-full"
+        className="object-cover rounded-xl h-full"
       />
-
       {active === "1" ? (
-        <div className="absolute text-white left-2 bottom-4 flex  flex-col gap-2">
-          <h1 className="text-3xl font-extrabold">{role}</h1>
-          <p className="text-xl">{name}</p>
+        <div
+          className={`        transform transition duration-500 ${
+            active === "1" ? "rotate-0" : "-rotate-90"
+          }
+ absolute text-white left-2 bottom-4 flex flex-col md:gap-2 `}>
+          <h1
+            style={{
+              transition: "transform 0.5s ease-in-out, left 0.5s ease-in-out",
+            }}
+            className="text-xl md:text-3xl font-extrabold">
+            {role}
+          </h1>
+          <p className="text-lg">{name}</p>
         </div>
       ) : (
         <>
-          <div className=" absolute left-0  bottom-8 w-16 h-16 bg-purple"></div>
-          <div className="absolute h-80 sm:-bottom-8 sm:left-[90px] md:-bottom-6 md:left-[96px] md2:-bottom-2 md2:left-16  lg:-bottom-10 lg:left-[96px] bottom-12 left-6 w-full   text-white -rotate-90   ">
-            <p className="text-lg font-extrabold text-nowrap ">{role}</p>
+          <div className="absolute left-0 bottom-8 w-16 h-16 bg-purple"></div>
+          <div
+            className={`absolute h-80 sm:-bottom-8 sm:left-[90px] md:-bottom-6 md:left-[96px] md2:-bottom-2 md2:left-50 lg:-bottom-10 lg:left-[96px] -bottom-12 left-[100%] w-full text-white ${
+              active === "1" ? "rotate-0" : "-rotate-90"
+            }`}
+            style={{
+              transform: `rotate(${active === "1" ? "0deg" : "-90deg"})`,
+              transition: "transform 0.5s ease-in-out, left 0.5s ease-in-out",
+            }}>
+            <p
+              className="text-sm md:text-lg font-extrabold text-nowrap"
+              style={{
+                transition: "transform 0.5s ease-in-out",
+              }}>
+              {role}
+            </p>
           </div>
         </>
       )}
--- src/components/goalsAndVision/Card.jsx
+++ src/components/goalsAndVision/Card.jsx
@@ -1,13 +1,24 @@
 import React from "react";
 import { motion } from "framer-motion";
+
 const Card = ({ goal }) => {
   return (
-    <div className="px-[10px] md:px-[30px] py-[20px] z-0 mx-auto relative w-[175.1px] md:w-[250px] lg:w-[300px] xl:w-[380px] h-[120px] md:h-[200px]  perspective my-[20px] md:my-[42px]">
-      <motion.div className="z-0 mx-auto w-full h-full relative transition-transform duration-300">
+    <div className="px-[10px] md:px-[30px] py-[20px] z-0 mx-auto relative w-[175.1px] md:w-[250px] lg:w-[300px] xl:w-[380px] h-[120px] md:h-[200px] perspective">
+      <motion.div
+        className="z-0 mx-auto w-full h-full relative transition-transform duration-100"
+        whileHover={{
+          translateY: -20, // Translate up by 20px on hover
+        }}>
         <motion.div
-          style={{ boxShadow: "0px 2.19px 2.19px 0px #00000040" }}
-          className="w-full h-full bg-purple-100 md:rounded-[10px] rounded-[5.09px] flex text-purple  flex-col items-center justify-center p-[10.7px] lg:p-[15px] xl:p-[21px] backface-hidden">
-          <p className="text-[13.28px] md:text-[18px] xl:text-[24px] font-montserrat font-[800] ">
+          style={{
+            boxShadow: "0px 2.19px 2.19px 0px #00000040",
+            backgroundColor: "#CCBAFE", // Initial background color
+          }}
+          whileHover={{
+            backgroundColor: "#B89EFD", // Change background color on hover
+          }}
+          className="w-full h-full md:rounded-[10px] rounded-[5.09px] flex text-purple flex-col items-center justify-center p-[10.7px] lg:p-[15px] xl:p-[21px] backface-hidden">
+          <p className="text-[13.28px] md:text-[18px] xl:text-[24px] font-montserrat font-[800]">
             {goal.title}
           </p>
           <div className="md:pt-[25px] flex relative w-full items-center justify-center">
