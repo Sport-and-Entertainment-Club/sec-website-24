38f540bab5b0b54c4bc2602e8fcf69139e3ce18c
wissalcodes <lw_messikh@esi.dz>
2024-10-18 16:08:33 +0100

    enabled buttons

--- src/components/JoinUs.jsx
+++ src/components/JoinUs.jsx
@@ -19,9 +19,8 @@ const JoinUs = () => {
           Join our community for fun, learning, and lasting connections!
         </h1>
         <button
-          disabled
           onClick={handleJoinClick}
-          className="opacity-70 cursor-not-allowed z-30 w-[32vw] max-w-[300.52px] md:mt-[50px] bg-pink text-white font-montserrat font-bold text-[15px] py-2 md:py-5 px-7 md:px-4 rounded-[9.26px] md:rounded-[20px] text-lg md:text-[32px] md:w-[20vw] md:max-w-[222px]">
+          className=" z-30 w-[32vw] max-w-[300.52px] md:mt-[50px] bg-pink text-white font-montserrat font-bold text-[15px] py-2 md:py-5 px-7 md:px-4 rounded-[9.26px] md:rounded-[20px] text-lg md:text-[32px] md:w-[20vw] md:max-w-[222px]">
           <Link> Join us</Link>
         </button>
       </div>
