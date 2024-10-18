7cee039f0d8f158214958084d6ac0d99a8d281a0
wissalcodes <lw_messikh@esi.dz>
2024-10-18 02:16:03 +0100

    updated blueprint

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        // You can optimize chunking if necessary
-        manualChunks: undefined,
-      },
-    },
-    // This helps avoid the chunk size warnings in build
-    chunkSizeWarningLimit: 1500, // Adjust as necessary
-  },
-  server: {
-    // Ensures that Vite will fallback to `index.html` for SPA routing
-    historyApiFallback: true,
-  },
-  // Optional: Customize public path if deploying to a subdirectory
-  base: "/",
 });
